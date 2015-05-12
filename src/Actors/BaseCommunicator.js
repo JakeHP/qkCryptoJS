8d54a6e57c42616fa5bb204a5d742e8f9ca98af2
Jake Prather <JakeHP@Zoho.com>
2015-05-11 20:28:46 -0500

    BaseComm lint fixes.:

--- src/Actors/BaseCommunicator.js
+++ src/Actors/BaseCommunicator.js
@@ -5,48 +5,50 @@ import {PhotonsSize} from "../Config/AppConfig.js";
 
 export var getBaseCommunicator = (() => {
 
-    var randomBasis = [];
-    var otherBasis = [];
-    var sharedKey = [];
-    var photons = [];
-    //var decision = undefined;
+    var randomBasis = [],
+        otherBasis = [],
+        sharedKey = [],
+        photons = [];
+    //decision = undefined;
     //var otherDecision = undefined;
 
-    function* basisGenerator(){
-        var i=0;
-        while(i<PhotonsSize){
+    //jscs:disable
+    function* basisGenerator() {
+        var i = 0;
+        while (i < PhotonsSize) {
             i++;
             yield (Math.floor(Math.random() * (2)) === 1) ? Diagonal : Rectangular;
         }
     }
+    //jscs:enable
 
-    function generateRandomBasis(){
+    function generateRandomBasis() {
         this.randomBasis = [];
-        var gen = basisGenerator();
-        var i = gen.next().value;
-        while(i !== undefined){
+        var gen = basisGenerator(),
             i = gen.next().value;
-            if(i !== undefined){
+        while (i !== undefined) {
+            i = gen.next().value;
+            if (i !== undefined) {
                 this.randomBasis.push(i);
             }
         }
     }
 
-    function isValidChannel(channel){
-        if(channel === undefined || channel.BasisUsed === undefined || channel.Photons === undefined){
+    function isValidChannel(channel) {
+        if (channel === undefined || channel.BasisUsed === undefined || channel.Photons === undefined) {
             throw 'BaseCommunicator.js - isValidChannel() - Invalid channel provided.';
         }
         return true;
     }
 
-    function readBasisFromChannel(channel){
-        if(isValidChannel(channel)){
+    function readBasisFromChannel(channel) {
+        if (isValidChannel(channel)) {
             this.otherBasis = channel.BasisUsed.slice(0);
         }
     }
 
-    function sendBasisToChannel(channel){
-        if(isValidChannel(channel)){
+    function sendBasisToChannel(channel) {
+        if (isValidChannel(channel)) {
             channel.BasisUsed = this.randomBasis.slice(0);
         }
     }
